a5a1de9068276c882d7f74d13461007c3326e45a
gwebsurfer <gab@gab.art.br>
2024-06-03 19:56:29 -0300

    feat: adjust services

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -1,13 +1,28 @@
+import { showToast } from '../components/toast/toast';
+
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function getProducts(endpoint) {
+  try {
     const response = await fetch(`${BASE_URL}/${endpoint}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': API_KEY,
-        },
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': API_KEY,
+      },
     });
-    return response.json();
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    showToast(
+      `Failed to retrieve the list of ${endpoint}. Please try again.`,
+      'error'
+    );
+  }
 }
--- src/services/postOrder.js
+++ src/services/postOrder.js
@@ -1,14 +1,28 @@
+import { showToast } from '../components/toast/toast.js';
+
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function postOrder(data) {
+  try {
     const response = await fetch(`${BASE_URL}/order`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': API_KEY,
-        },
-        body: JSON.stringify(data),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': API_KEY,
+      },
+      body: JSON.stringify(data),
     });
-    return response.json();
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    //showToast('Order successfully placed', 'success');
+
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    showToast('Failed to place order. Please try again.', 'error');
+  }
 }
